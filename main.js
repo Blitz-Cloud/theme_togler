425b7928ae60509d37aab7ec0e83cec2f9df9646
Ionut <81566145+Blitz-Cloud@users.noreply.github.com>
2023-04-02 16:08:12 +0300

    Update[html,css]: I deleted some unnecessary styles and elements

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
-const container = document.getElementById("container");
+const body = document.querySelector("body");
 const dark_btn = document.getElementById("dark_btn");
 const light_btn = document.getElementById("light_btn");
 
 function themeToggle() {
-  if (container.classList.contains("theme")) {
-    container.classList.toggle("theme");
+  if (body.classList.contains("theme")) {
+    body.classList.toggle("theme");
     light_btn.classList.add("hidden");
     dark_btn.classList.remove("hidden");
     // pt fiecare din cele 2 cazuri ar trebui salvat in localStorage
     // thema preferata de utilizator
   } else {
-    container.classList.toggle("theme");
+    body.classList.toggle("theme");
     light_btn.classList.remove("hidden");
     dark_btn.classList.add("hidden");
     // pt fiecare din cele 2 cazuri ar trebui salvat in localStorage
