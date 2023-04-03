f484ce5eefcf9b7d9a646913a4d495e7568ac9c5
Ionut <81566145+Blitz-Cloud@users.noreply.github.com>
2023-04-03 19:05:41 +0300

    Update[theme_controler]: Now the theme btn cicles throughth all states
    I modified the old js controller to cicle throughth all states.

--- main.js
+++ main.js
@@ -2,27 +2,27 @@ const body = document.querySelector("body");
 const dark_btn = document.getElementById("dark_btn");
 const light_btn = document.getElementById("light_btn");
 const system_theme = document.getElementById("system_theme");
+const themeBtn = document.querySelector("button").children;
 
+let i = 0;
 function themeToggle() {
-  system_theme.classList.add("hidden");
-  if (body.classList.contains("theme")) {
-    // trebuie adaugat local-storage state managment
-    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-      console.log("Tema preferata este Dark");
+  themeBtn[i].classList.toggle("hidden");
+  switch (i) {
+    case 0:
       body.classList = "light_theme";
-      dark_btn.classList.toggle("hidden");
-    } else {
-      console.log("Tema preferata este Light");
+      break;
+    case 1:
       body.classList = "dark_theme";
-      light_btn.classList.toggle("hidden");
-    }
-  } else if (body.classList.contains("light_theme")) {
-    body.classList = "dark_theme";
-    dark_btn.classList.toggle("hidden");
-    light_btn.classList.toggle("hidden");
-  } else {
-    body.classList = "light_theme";
-    dark_btn.classList.toggle("hidden");
-    light_btn.classList.toggle("hidden");
+      break;
+    case 2:
+      body.classList = "theme";
+      break;
   }
+  if (i == 2) {
+    i = 0;
+  } else {
+    i++;
+  }
+
+  themeBtn[i].classList.toggle("hidden");
 }
