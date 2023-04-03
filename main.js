af8c281fa0bd63cb5274a57655e4021f4fdf9de3
Ionut <81566145+Blitz-Cloud@users.noreply.github.com>
2023-04-03 18:21:15 +0300

    Update[js]: Theme controler fixed
    I need to also implement some local-storage controler for the theme and
    the theme btn should cicle throughth all btn

--- main.js
+++ main.js
@@ -1,19 +1,28 @@
 const body = document.querySelector("body");
 const dark_btn = document.getElementById("dark_btn");
 const light_btn = document.getElementById("light_btn");
+const system_theme = document.getElementById("system_theme");
 
 function themeToggle() {
+  system_theme.classList.add("hidden");
   if (body.classList.contains("theme")) {
-    body.classList.toggle("theme");
-    light_btn.classList.add("hidden");
-    dark_btn.classList.remove("hidden");
-    // pt fiecare din cele 2 cazuri ar trebui salvat in localStorage
-    // thema preferata de utilizator
+    // trebuie adaugat local-storage state managment
+    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+      console.log("Tema preferata este Dark");
+      body.classList = "light_theme";
+      dark_btn.classList.toggle("hidden");
+    } else {
+      console.log("Tema preferata este Light");
+      body.classList = "dark_theme";
+      light_btn.classList.toggle("hidden");
+    }
+  } else if (body.classList.contains("light_theme")) {
+    body.classList = "dark_theme";
+    dark_btn.classList.toggle("hidden");
+    light_btn.classList.toggle("hidden");
   } else {
-    body.classList.toggle("theme");
-    light_btn.classList.remove("hidden");
-    dark_btn.classList.add("hidden");
-    // pt fiecare din cele 2 cazuri ar trebui salvat in localStorage
-    // thema preferata de utilizator
+    body.classList = "light_theme";
+    dark_btn.classList.toggle("hidden");
+    light_btn.classList.toggle("hidden");
   }
 }
